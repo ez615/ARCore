b0f7bb68fc0a5c43aa068de92c022667fab465cc
Jihyun <touchjh01@naver>
2021-06-17 03:02:26 +0900

    AR Core practice

---
+++ app/src/main/java/com/example/arcore_2/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.arcore_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.SkeletonNode;
+import com.google.ar.sceneform.animation.ModelAnimator;
+import com.google.ar.sceneform.rendering.AnimationData;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ModelAnimator modelAnimator;
+    private int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ArFragment arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+       arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            createModel(hitResult.createAnchor(), arFragment);
+       });
+    }
+
+    @TargetApi(24)
+    private void createModel(Anchor anchor, ArFragment arFragment) {
+        ModelRenderable
+                .builder()
+                .setSource(this, Uri.parse("skeleton.sfb"))
+                .build()
+                .thenAccept(modelRenderable -> {
+
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+
+                    SkeletonNode skeletonNode = new SkeletonNode();
+                    skeletonNode.setParent(anchorNode);
+                    skeletonNode.setRenderable(modelRenderable);
+
+                    arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+                    Button button = findViewById(R.id.button);
+
+                    button.setOnClickListener(view -> animateModel(modelRenderable));
+                });
+    }
+
+    private void animateModel(ModelRenderable modelRenderable) {
+        if(modelAnimator != null && modelAnimator.isRunning()){
+            modelAnimator.end();
+        }
+
+        int animationCount = modelRenderable.getAnimationDataCount();
+
+        if(i == animationCount){
+            i = 0;
+        }
+
+        AnimationData animationData = modelRenderable.getAnimationData(i);
+
+        modelAnimator = new ModelAnimator(animationData, modelRenderable);
+        modelAnimator.start();
+
+        i++;
+    }
+}
